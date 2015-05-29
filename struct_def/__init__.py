3b2cc5e3b74f216d83e9ec19175dedd69a36f4c0
bropony <ahda@qq.com>
2015-05-29 16:06:13 +0800

    commit

--- struct_def/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-* @name __init__.py
-*
-* @author ahda86@gmail.com
-*
-* @date 2015/5/27 16:15
-*
-* @desc __init__.py
-"""
--- struct_def/gamit/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-* @name __init__.py
-*
-* @author ahda86@gmail.com
-*
-* @date 2015/5/27 17:03
-*
-* @desc __init__.py
-"""
