4a1e6e919c5c7d69a0aaed341230a5716e666756
bropony <ahda@qq.com>
2015-06-18 17:10:39 +0800

    gmt2cpp

--- autotools/gmt2cpp.py
+++ autotools/gmt2cpp.py
@@ -142,6 +142,7 @@ class Gmt2Cpp:
 
         self.writeHpp("#include <map>")
         self.writeHpp("#include <vector>")
+        self.writeHpp("#include <gamit/util/autorun.h>")
         self.writeHpp("#include <gamit/util/sharedptr.h>")
         self.writeHpp("#include <gamit/serialize/serializer.h>")
         self.writeHpp("#include <gamit/rmi/RmiCore.h>")
@@ -152,16 +153,16 @@ class Gmt2Cpp:
             self.writeHpp("#include <{}.h>".format("/".join(fields)))
         self.writeHpp()
 
-        self.writeCpp("#include <.h>".format("/".join(self.scopes)))
+        self.writeCpp("#include <{}.h>".format("/".join(self.scopes)))
         self.writeCpp()
 
     def begin(self):
         for scope in self.scopes:
             self.writeHpp("namespace {}{}".format(scope, "{"))
             self.hppIndent += 1
+        self.writeHpp()
 
     def end(self):
-        self.writeHpp()
         for scope in self.scopes:
             self.hppIndent -= 1
             self.writeHpp("}")
@@ -180,35 +181,426 @@ class Gmt2Cpp:
             elif dataType.name == "binary":
                 return "std::string"
             elif dataType.name == "date":
-                return "gamit::CDatetime"
+                return "gamit::CDateTime"
             else:
                 return dataType.name
         else:
             return dataType.fullname.replace(".", "::")
 
+    def getRefForm(self, dataType):
+        if isinstance(dataType, BasicType):
+            if dataType.name == "string" or dataType.name == "binary":
+                return "const std::string &"
+            elif dataType.name == "date":
+                return "const gamit::CDateTime &"
+            else:
+                return self.getCppType(dataType)
+        elif isinstance(dataType, Enum):
+            return dataType.fullname.replace(".", "::")
+        else:
+            return "const " + dataType.fullname.replace(".", "::") + " &"
+
+    def getReadExpr(self, varName, dataType):
+        res = "readExpr:Oops"
+        if isinstance(dataType, BasicType):
+            res = "__is.read({})".format(varName)
+        elif isinstance(dataType, Struct):
+            res = "{}.__read(__is)".format(varName)
+        elif isinstance(dataType, Dict) or isinstance(dataType, List):
+            scope = dataType.scope.replace(".", "::")
+            res = "{0}::__read(__is, {1}, {0}::__{2}_U__())".format(scope, varName, dataType.name)
+        elif isinstance(dataType, Enum):
+            scope = dataType.scope.replace(".", "::")
+            res = "{}::read(__is, {})".format(scope, varName)
+
+        return res
+
+    def getWriteExpr(self, varName, dataType):
+        res = "writeExpr:Oops"
+        if isinstance(dataType, BasicType):
+            res = "__os.write({})".format(varName)
+        elif isinstance(dataType, Struct):
+            res = "{}.__write(__os)".format(varName)
+        elif isinstance(dataType, List) or isinstance(dataType, Dict):
+            scope = dataType.scope.replace(".", "::")
+            res = "{0}::__write(__os, {1}, {0}::__{2}_U__())".format(scope, varName, dataType.name)
+        elif isinstance(dataType, Enum):
+            scope = dataType.scope.replace(".", "::")
+            res = "{}::write(__os, {})".format(scope, varName)
+
+        return  res
+
     def parseStruct(self, dataType):
         structName = dataType.name
+        classScope = "{}::{}".format(self.cppScope, structName)
+        self.writeHpp("//{}".format(structName))
+        self.writeHpp("class {};".format(structName))
+        self.writeHpp("typedef std::CSharedPtr<{0}> {0}Ptr;".format(structName))
         self.writeHpp("class {}: public gamit::MessageBase".format(structName))
         self.writeHpp("{") # begin of class
         self.writeHpp("public:")
         self.hppIndent += 1
 
+        self.writeCpp("// implementation of class {}".format(structName))
+        self.writeCpp("std::string {}::_msgName(\"{}\");".format(classScope, structName))
+        self.writeCpp()
+
         for field in dataType.fields:
             self.writeHpp("{} {};".format(self.getCppType(field.type), field.name))
 
-        self.hppIndent -= 1 # end of class
-        self.writeHpp("};")
+        # regist
+        self.writeHpp()
+        self.writeHpp("static void regist();")
+        self.writeHpp("static const {}Ptr create();".format(structName))
         self.writeHpp()
 
+        # regist func
+        self.writeCpp("void {}::regist()".format(classScope))
+        self.writeCpp("{")
+        self.cppIndent += 1
+        self.writeCpp('gamit::MessageBlock::registCreateFunc("{0}", {0}::create);'.format(structName))
+        self.cppIndent -= 1
+        self.writeCpp("}")
+        self.writeCpp()
+
+        # create func
+        self.writeCpp("const {}::{}Ptr {}::create()".format(self.cppScope, structName, classScope))
+        self.writeCpp("{")
+        self.cppIndent += 1
+        self.writeCpp("return (new {}());".format(structName))
+        self.cppIndent -= 1
+        self.writeCpp("}")
+        self.writeCpp()
+
+        # read and write
+        self.writeHpp("virtual void __read(gamit::CSerializer & __is);")
+        self.writeHpp("virtual void __write(gamit::CSerializer & __os) const;")
+        self.writeHpp("virtual const std::string & __name() const;")
+        self.writeHpp()
+
+        # __read
+        self.writeCpp("void {}::__read(gamit::CSerializer & __is)".format(classScope))
+        self.writeCpp("{")
+        self.cppIndent += 1
+        for field in dataType.fields:
+            readExpr = self.getReadExpr(field.name, field.type)
+            self.writeCpp("{};".format(readExpr))
+        self.cppIndent -= 1
+        self.writeCpp("}")
+        self.writeCpp()  # end of __read
+
+        # __write
+        self.writeCpp("void {}::__write(gamit::CSerializer & __os) const".format(classScope))
+        self.writeCpp("{")
+        self.cppIndent += 1
+        for field in dataType.fields:
+            writeExpr = self.getWriteExpr(field.name, field.type)
+            self.writeCpp("{};".format(writeExpr))
+        self.cppIndent -= 1
+        self.writeCpp("}")
+        self.writeCpp()  # end of __write
+
+        # __name
+        self.writeCpp("const std::string & {}::__name() const".format(classScope))
+        self.writeCpp("{")
+        self.cppIndent += 1
+        self.writeCpp("return _msgName;")
+        self.cppIndent -= 1
+        self.writeCpp("}")
+        self.writeCpp()
+
+        # private
+        self.hppIndent -= 1
+        self.writeHpp("private:")
+        self.hppIndent += 1
+        self.writeHpp("static std::string _msgName;")
+
+        self.hppIndent -= 1 # end of class
+        self.writeHpp("{}; // class {}".format("}", structName))
+        self.writeHpp("gamit::CAutoRun regist{0}({0}::regist);".format(structName))
+        self.writeHpp()
+
+    def parseEnum(self, dataType):
+        enumName = dataType.name
+        enumFullName = dataType.fullname.replace(".", "::")
+        self.writeHpp("// enum class {}".format(enumName))
+        self.writeHpp("enum class {}".format(enumName))
+        self.writeHpp("{")
+        self.hppIndent += 1
+        for pair in dataType.pairs:
+            self.writeHpp("{} = {},".format(*pair))
+        self.hppIndent -= 1
+        self.writeHpp("};")
+        self.writeHpp("void read(gamit::CSerializer & __is, {} & __val);".format(enumName))
+        self.writeHpp("void write(gamit::CSerializer & __os, {} __val);".format(enumName))
+        self.writeHpp()
+
+        self.writeCpp("void {}::read(gamit::CSerializer & __is, {} &__val)".format(self.cppScope, enumFullName))
+        self.writeCpp("{")
+        self.cppIndent += 1
+        self.writeCpp("int __i = 0;")
+        self.writeCpp("__is.read(__i);")
+        self.writeCpp("__val = {}(__i);".format(enumFullName))
+        self.cppIndent -= 1
+        self.writeCpp("}")
+        self.writeCpp()
+
+        self.writeCpp("void {}::write(gamit::CSerializer & __os, {} __val)".format(self.cppScope, enumFullName))
+        self.writeCpp("{")
+        self.cppIndent += 1
+        self.writeCpp("__os.write(int(__val));")
+        self.cppIndent -= 1
+        self.writeCpp("}")
+        self.writeCpp()
+
     def parseList(self, dataType):
-        pass
+        listName = dataType.name
+        listFullName = dataType.fullname.replace(".", "::")
+        typeName = self.getCppType(dataType.type)
+        uniqueClassName = "__{}_U__".format(listName)
+
+        self.writeHpp("// {}".format(listName))
+        self.writeHpp("typedef std::vector< {} > {};".format(self.getCppType(dataType.type), listName))
+        self.writeHpp("class {}{};".format(uniqueClassName, "{}"))
+        self.writeHpp("void __read(gamit::CSerializer & __is, {} & __valList, {});".format(listName, uniqueClassName))
+        self.writeHpp("void __write(gamit::CSerializer & __os, const {} &__valList, {});".format(listName, uniqueClassName))
+        self.writeHpp()
+
+        self.writeCpp(
+            "void {}::__read(gamit::CSerializer & __is, {}::{} & __valList, {})".format(
+                self.cppScope, self.cppScope, listName, uniqueClassName))
+        self.writeCpp("{")
+        self.cppIndent += 1
+        self.writeCpp("int __size = 0;")
+        self.writeCpp("__is.read(__size);")
+        self.writeCpp("for (int i = 0; i < __size; ++i)")
+        self.writeCpp("{")
+        self.cppIndent += 1
+        self.writeCpp("{} __val;".format(typeName))
+        readExpr = self.getReadExpr("__val", dataType.type)
+        self.writeCpp("{};".format(readExpr))
+        self.writeCpp("__valList.push_back(__val);")
+        self.cppIndent -= 1
+        self.writeCpp("}")
+        self.cppIndent -= 1
+        self.writeCpp("}")
+        self.writeCpp()
+
+        self.writeCpp(
+            "void {}::__write(gamit::CSerializer & __os, const {}::{} & __valList, {})".format(
+                self.cppScope, self.cppScope, listName, uniqueClassName))
+        self.writeCpp("{")
+        self.cppIndent += 1
+        self.writeCpp("__os.writeSize(__valList.size());")
+        self.writeCpp("for (auto __val: __valList)")
+        self.writeCpp("{")
+        self.cppIndent += 1
+        writeExpr = self.getWriteExpr("__val", dataType.type)
+        self.writeCpp(writeExpr + ";")
+        self.cppIndent -= 1
+        self.writeCpp("}")
+        self.cppIndent -= 1
+        self.writeCpp("}")
+        self.writeCpp()
 
     def parseDict(self, dataType):
-        pass
+        dictName = dataType.name
+        dictFullName = dataType.fullname.replace(".", "::")
+        keyName = self.getCppType(dataType.keyType)
+        valName = self.getCppType(dataType.valType)
+        uniqueClassName = "__{}_U__".format(dictName)
+
+        self.writeHpp("// {}".format(dictName))
+        self.writeHpp("typedef std::map< {}, {} > {};".format(keyName, valName, dictName))
+        self.writeHpp("class {}{};".format(uniqueClassName, "{}"))
+
+        self.writeHpp("void __read(gamit::CSerializer & __is, {} & __valDict, {});".format(dictName, uniqueClassName))
+        self.writeHpp("void __write(gamit::CSerializer & __os, const {} &__valDict, {});".format(dictName, uniqueClassName))
+        self.writeHpp()
+
+        self.writeCpp(
+            "void {}::__read(gamit::CSerializer & __is, {} & __valDict, {})".format(
+                self.cppScope, dictFullName, uniqueClassName))
+        self.writeCpp("{")
+        self.cppIndent += 1
+        self.writeCpp("int __size = 0;")
+        self.writeCpp("__is.read(__size);")
+        self.writeCpp("for (int i = 0; i < __size; ++i)")
+        self.writeCpp("{")
+        self.cppIndent += 1
+        self.writeCpp("{} __key;".format(keyName))
+        self.writeCpp("{} __val;".format(valName))
+        readKeyExpr = self.getReadExpr("__key", dataType.keyType)
+        readValExpr = self.getReadExpr("__val", dataType.valType)
+        self.writeCpp(readKeyExpr + ";")
+        self.writeCpp(readValExpr + ";")
+        self.writeCpp("__valDict.insert({__key, __val});")
+        self.cppIndent -= 1
+        self.writeCpp("}")
+        self.cppIndent -= 1
+        self.writeCpp("}")
+        self.writeCpp()
+
+        self.writeCpp(
+            "void {}::__write(gamit::CSerializer & __os, const {} & __valDict, {})".format(
+                self.cppScope, dictFullName, uniqueClassName))
+        self.writeCpp("{")
+        self.cppIndent += 1
+        self.writeCpp("__os.writeSize(__valDict.size());")
+        self.writeCpp("for (auto __pair: __valDict)")
+        self.writeCpp("{")
+        self.cppIndent += 1
+        writeKeyExpr = self.getWriteExpr("__pair.first", dataType.keyType)
+        writeValExpr = self.getWriteExpr("__pair.second", dataType.valType)
+        self.writeCpp(writeKeyExpr + ";")
+        self.writeCpp(writeValExpr + ";")
+        self.cppIndent -= 1
+        self.writeCpp("}")
+        self.cppIndent -= 1
+        self.writeCpp("}")
+        self.writeCpp()
 
     def parseInterface(self, dataType):
-        pass
+        for method in dataType.methodList:
+            self.parseResponse(dataType, method)
 
+        self.parseProxy(dataType)
+
+    def parseResponse(self, interfaceType, method):
+        className = "{}_{}_Response".format(interfaceType.name, method.name)
+        classFullName = "{}::{}".format(self.cppScope, className)
+        self.writeHpp("// {}".format(className))
+        self.writeHpp("class {}: public gamit::CRmiResponseBase".format(className))
+        self.writeHpp("{")
+        self.hppIndent += 1
+        self.writeHpp("virtual void __onResponse(gamit::CSerializer & __is);")
+        self.writeHpp("virtual void __onError(gamit::CSerializer & __is);")
+        self.writeHpp("virtual void __onTimeout();")
+        self.writeHpp()
+        self.fhpp.write(self._getHppIndent())
+        self.fhpp.write("virtual void onResponse(")
+        isFirst = True
+        for field in method.outfields:
+            if isFirst:
+                isFirst = False
+            else:
+                self.fhpp.write(",")
+
+            ref = self.getRefForm(field.type)
+            self.fhpp.write("{} {}".format(ref, field.name))
+
+        self.fhpp.write(") = 0;\n")
+        self.writeHpp("virtual void onError(const std::string & what, int code) = 0;")
+        self.writeHpp("virtual void onTimeout() = 0;")
+        self.hppIndent -= 1
+        self.writeHpp("{}; // class {}".format("}", className))
+        self.writeHpp("typedef std::CSharedPtr<{0}> {0}Ptr;".format(className))
+        self.writeHpp()
+
+        self.writeCpp("void {}::__onResponse(gamit::CSerializer & __is)".format(classFullName))
+        self.writeCpp("{")
+        self.cppIndent += 1
+        for field in method.outfields:
+            self.writeCpp("{} {};".format(self.getCppType(field.type), field.name))
+            self.writeCpp("{};".format(self.getReadExpr(field.name, field.type)))
+        self.writeCpp("onResponse(", False)
+        isFirst = True
+        for field in method.outfields:
+            if isFirst:
+                isFirst = False
+            else:
+                self.fcpp.write(",")
+            self.fcpp.write(field.name)
+        self.fcpp.write(");\n")
+        self.cppIndent -= 1
+        self.writeCpp("}")
+        self.writeCpp()
+
+        self.writeCpp("void {}::__onError(gamit::CSerializer & __is)".format(classFullName))
+        self.writeCpp("{")
+        self.cppIndent += 1
+        self.writeCpp("std::string __what;")
+        self.writeCpp("__is.read(__what);")
+        self.writeCpp("int __code;")
+        self.writeCpp("__is.read(__code);")
+        self.writeCpp("onError(__what, __code);")
+        self.cppIndent -= 1
+        self.writeCpp("}")
+        self.writeCpp()
+
+        self.writeCpp("void {}::__onTimeout()".format(classFullName))
+        self.writeCpp("{")
+        self.cppIndent += 1
+        self.writeCpp("onTimeout();")
+        self.cppIndent -= 1
+        self.writeCpp("}")
+        self.writeCpp()
+
+    def parseProxy(self, interfaceType):
+        className = interfaceType.name
+        classFullName = interfaceType.fullname.replace(".", "::")
+
+        self.writeCpp('std::string {}::__proxyName("{}");'.format(classFullName, className))
+        self.writeCpp()
+
+        self.writeHpp("// {}".format(className))
+        self.writeHpp("class {}: public gamit::CRmiProxyBase".format(className))
+        self.writeHpp("{")
+        self.writeHpp("public:")
+        self.hppIndent += 1
+
+        for method in interfaceType.methodList:
+            self.parseCallout(className, classFullName, method)
+
+        self.writeHpp()
+        self.hppIndent -= 1
+        self.writeHpp("private:")
+        self.hppIndent += 1
+        self.writeHpp("static std::string __proxyName;")
+
+        self.hppIndent -= 1
+        self.writeHpp("{}; // class {}".format("}", className))
+        self.writeHpp("typedef std::CSharedPtr<{}> {}Ptr;".format(className, className))
+        self.writeHpp()
+
+    def parseCallout(self, className, classFullName, method):
+        responseName = "{}_{}_ResponsePtr".format(className, method.name)
+        self.writeHpp("void {}(const {} &".format(method.name, responseName), False)
+        for field in method.infields:
+            self.fhpp.write(", {} {}".format(self.getRefForm(field.type), field.name))
+        self.fhpp.write(");\n")
+
+        self.writeCpp("void {}::{}(const {} & __cb".format(classFullName, method.name, responseName), False)
+        for field in method.infields:
+            self.fcpp.write(", {} {}".format(self.getRefForm(field.type), field.name))
+        self.fcpp.write(")\n")
+        self.writeCpp("{")
+        self.cppIndent += 1
+
+        self.writeCpp('static std::string __methodName = "{}";'.format(method.name))
+        self.writeCpp()
+        self.writeCpp("gamit::CSerializer __os;")
+        self.writeCpp("__os.startToWrite();")
+        self.writeCpp()
+        self.writeCpp("__os.write(int(gamit::ERmiType::RmiCall));")
+        self.writeCpp('__os.write(__proxyName);')
+        self.writeCpp("__os.write(__methodName);")
+        self.writeCpp()
+        self.writeCpp("int __msgId = __getMsgId();")
+        self.writeCpp("__os.write(__msgId);")
+        self.writeCpp("__cb->setMsgId(__msgId);")
+        self.writeCpp()
+        for field in method.infields:
+            writeExpr = self.getWriteExpr(field.name, field.type)
+            self.writeCpp(writeExpr + ";")
+
+        if method.infields:
+            self.writeCpp()
+
+        self.writeCpp("invoke(__os, __cb);")
+        self.cppIndent -= 1
+        self.writeCpp("}")
+        self.writeCpp()
 
 def main():
     parser = OptionParser()
